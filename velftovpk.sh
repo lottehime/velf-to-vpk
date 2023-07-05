ef1b048e08cc29562d6d73edd8c8df0d0c132da5
Lotte <joshdoes3d@gmail.com>
2023-07-05 12:10:23 +1000

    Update velftovpk.sh

--- velftovpk.sh
+++ velftovpk.sh
@@ -6,5 +6,5 @@ vita-mksfoex -s TITLE_ID=$2 "$1" param.sfo
 mkdir sce_sys
 mv param.sfo ./sce_sys/param.sfo
 zip -r $1.vpk eboot.bin ./sce_sys
-echo 'Done packing. You should add a template, images, etc.'
-read -n1 -r -p "I know, I am being lazy... [Press any key]"
+echo 'Done packing. Maybe add a template and images later?'
+read -n1 -r -p "Yeah maybe... [Press any key]"
